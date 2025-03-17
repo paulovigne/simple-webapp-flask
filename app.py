7296f0e0329cae41c05b5a3dc72ea182fd6b5d67
paulovigne <57726813+paulovigne@users.noreply.github.com>
2025-03-17 20:49:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Hello World Build!!!</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Hello World Build Full Grant !!!</p></h1>'
 
 @app.route('/api')
 def hello():
